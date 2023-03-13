e5cfa4e7c405df247e72d2a67e45c68607c2a297
Marcos Paulo Holanda <marcos.holanda@hotmail.com.br>
2023-03-13 13:48:58 -0300

    .

--- insert_data_on_db.py
+++ insert_data_on_db.py
@@ -39,18 +39,17 @@ def insert_data_diesel(table_name, data_tuple):
     conn.commit()
     
 
-df = pd.read_excel('historico2.xlsx', header=0)
+df = pd.read_excel('historico.xlsx', header=0)
 df.data = df.data.dt.strftime('%d/%m/%Y')
 
-for i in range(11, len(df)):
+for i in range(0, len(df)+1):
     tupla = []
     for x in df.loc[i].values:
         if type(x) != str:
             tupla.append(str(round(x,4)))
         else:
             tupla.append(x)
-    insert_data_diesel('bd_diesel_s10', tupla)
-    print(i)
+    insert_data('bd_oc', tupla)
 
 
 
