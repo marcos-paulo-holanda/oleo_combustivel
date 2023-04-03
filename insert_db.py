31fa6319e2f184f522d2c4e00b7ea5fe5c05b779
Marcos Paulo Holanda <marcos.holanda@hotmail.com.br>
2023-04-03 13:31:39 -0300

    .

--- insert_db.py
+++ insert_db.py
@@ -64,12 +64,9 @@ def insert_same(tabela_bd: str):
         "DROP TEMPORARY TABLE IF EXISTS tmptable;"
         
 if __name__ == "__main__":
-    tarefa = int(input('Digite 0 replicar registros, 1 para inserir novos registros: '))
-    if tarefa == 0:
-        pass
-    elif tarefa == 1:
-        insert_db('historico_oc', insert_oc)
-        insert_db('historico_s10', insert_diesel)
-        insert_db('commodity', insert_commodity)
+    
+    insert_db('historico_oc', insert_oc)
+    insert_db('historico_s10', insert_diesel)
+    insert_db('commodity', insert_commodity)
 
 print('--Mission completed -- ')
